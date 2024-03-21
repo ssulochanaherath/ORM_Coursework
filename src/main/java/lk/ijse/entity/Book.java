a03eb7e273b3a40ace33984fed8f7c4c0988b9af
sangeeth <ssulochanaherath@gmail.com>
2024-03-21 22:23:09 +0530

    Project Completed.

--- src/main/java/lk/ijse/entity/Book.java
+++ src/main/java/lk/ijse/entity/Book.java
@@ -17,7 +17,6 @@ public class Book {
     private String status;
     @OneToMany(mappedBy = "book",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     private List<BorrowedBooks> borrowedBooks;
-
     public Book() {
     }
 
